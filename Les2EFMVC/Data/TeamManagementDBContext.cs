733649fdfbb083aa14088730db3bf6340d89bb79
Paul de Mast <pjf.demast@avans.nl>
2022-02-10 13:28:59 +0100

    update data

--- Les2EFMVC/Data/TeamManagementDBContext.cs
+++ Les2EFMVC/Data/TeamManagementDBContext.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Les2EFMVC.Data
 {
-    public class TeamManagementDBContext : DbContext {
+    public class TeamManagementDBContext : DbContext
+    {
         public TeamManagementDBContext(DbContextOptions<TeamManagementDBContext> options) : base(options) {
         }
 
@@ -19,15 +20,29 @@ namespace Les2EFMVC.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<Member>().HasData(
                 new Member { Id = 1001, Name = "Henk", Email = "henk@breda.nl", TeamId = 201 },
-                new Member { Id = 1002, Name = "Loes", Email = "loes@avans.nl", TeamId = 201 },
-                new Member { Id = 1003, Name = "Job", Email = "job@gmail.com", TeamId = 202 }
-                );
+                new Member { Id = 1002, Name = "Tim", Email = "tim@breda.nl", TeamId = 201 },
+                new Member { Id = 1003, Name = "Robbert", Email = "robbert@breda.nl", TeamId = 201 },
+                new Member { Id = 1004, Name = "Paul", Email = "paul@breda.nl", TeamId = 201 },
+                new Member { Id = 1005, Name = "Niek", Email = "Bart-Jan@breda.nl", TeamId = 201 },
+                new Member { Id = 1006, Name = "Teun", Email = "Teun@breda.nl", TeamId = 201 },
+
+                new Member { Id = 1007, Name = "Bart", Email = "bart@avans.nl", TeamId = 202 },
+                new Member { Id = 1008, Name = "Joris", Email = "joris@avans.nl", TeamId = 202 },
+                new Member { Id = 1009, Name = "Danny", Email = "danny@avans.nl", TeamId = 202 },
+                new Member { Id = 1010, Name = "Lars", Email = "lars@avans.nl", TeamId = 202 },
+                new Member { Id = 1011, Name = "Rob", Email = "rob@avans.nl", TeamId = 202 },
+                new Member { Id = 1012, Name = "Dennis", Email = "dennis@avans.nl", TeamId = 202 },
+                new Member { Id = 1013, Name = "Daniël", Email = "daniel@avans.nl", TeamId = 202 }
+       );
 
             modelBuilder.Entity<Team>().HasData(
-                new Team { Id = 201, Group = "Groep-1", Title = "You snooze, you loose!" },
-                new Team { Id = 202, Group = "Groep-2", Title = "Nachos" },
-                new Team { Id = 203, Group = "Groep-3", Title = "3 front-end devs + 1 back-end dev" },
-                new Team { Id = 204, Group = "Groep-4", Title = "Cinavas" }
+                new Team { Id = 201, Group = "Groep-A", Title = "Alpha Wolf Squadron" },
+                new Team { Id = 202, Group = "Groep-B", Title = "Moviehub" },
+                new Team { Id = 203, Group = "Groep-H", Title = "The Cool beans" },
+                new Team { Id = 204, Group = "Groep-1", Title = "You snooze, you loose!" },
+                new Team { Id = 205, Group = "Groep-2", Title = "Nachos" },
+                new Team { Id = 206, Group = "Groep-3", Title = "3 front-end devs + 1 back-end dev" },
+                new Team { Id = 207, Group = "Groep-4", Title = "Cinavas" }
                 );
         }
     }
---
+++ Les2EFMVC/Migrations/20220210122628_First.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Les2EFMVC.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Group = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeamId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Members_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "Group", "Title" },
+                values: new object[,]
+                {
+                    { 201, "Groep-A", "Alpha Wolf Squadron" },
+                    { 202, "Groep-B", "Moviehub" },
+                    { 203, "Groep-H", "The Cool beans" },
+                    { 204, "Groep-1", "You snooze, you loose!" },
+                    { 205, "Groep-2", "Nachos" },
+                    { 206, "Groep-3", "3 front-end devs + 1 back-end dev" },
+                    { 207, "Groep-4", "Cinavas" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Members",
+                columns: new[] { "Id", "Email", "Name", "TeamId" },
+                values: new object[,]
+                {
+                    { 1001, "henk@breda.nl", "Henk", 201 },
+                    { 1002, "tim@breda.nl", "Tim", 201 },
+                    { 1003, "robbert@breda.nl", "Robbert", 201 },
+                    { 1004, "paul@breda.nl", "Paul", 201 },
+                    { 1005, "Bart-Jan@breda.nl", "Niek", 201 },
+                    { 1006, "Teun@breda.nl", "Teun", 201 },
+                    { 1007, "bart@avans.nl", "Bart", 202 },
+                    { 1008, "joris@avans.nl", "Joris", 202 },
+                    { 1009, "danny@avans.nl", "Danny", 202 },
+                    { 1010, "lars@avans.nl", "Lars", 202 },
+                    { 1011, "rob@avans.nl", "Rob", 202 },
+                    { 1012, "dennis@avans.nl", "Dennis", 202 },
+                    { 1013, "daniel@avans.nl", "Daniël", 202 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_TeamId",
+                table: "Members",
+                column: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
